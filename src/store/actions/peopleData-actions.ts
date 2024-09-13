c30746e31c2454da4f242e6c7007581639285270
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-13 13:55:13 +0200

    write thunk for fetching async data for peopleData slice

---
+++ src/store/actions/peopleData-actions.ts
@@ -0,0 +1,13 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchPeopleData = createAsyncThunk(
+    'peopleData/fetchPeopleData',
+    async () => {
+             const response: Response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+
+                return response.json();
+    });
