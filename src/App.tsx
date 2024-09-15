204c0f4a4bf23aacd9f69a2929e641b5fa9ac2a8
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-15 19:17:03 +0200

    Add function for fetching data, new UserManagementTable component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch } from './store/store.ts';
+import { fetchPeopleData } from "./store/actions/peopleData-actions.ts";
+import { UserManagementTable } from "./components/UserManagementTable.tsx";
+import classes from "./App.module.css";
 
 export const App: React.FC = () => {
-  return null;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPeopleData());
+  }, [dispatch]);
+
+  return (
+      <div className={classes.mainContainer}>
+        <UserManagementTable />
+      </div>
+  );
 }
 
 export default App
